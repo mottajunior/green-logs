fed8f728c32e656222d703c13e608a73455eba14
Jose Motta <jose.motta@tegra.com.br>
2020-06-30 11:01:07 -0300

    imeplemented gray log decorator

--- index.ts
+++ index.ts
@@ -1,17 +1,21 @@
 import Gelf from "gelf";
 
+
+
 export class GreenLogs {
     private gelf: Gelf
     private hideLog: boolean
     constructor(host: string, hideLog: boolean = false) {
         this.hideLog = hideLog
         this.gelf = new Gelf({ graylogHostname: host });
+        SetObjectLog(this.gelf)
         this.gelf.on('error', (err: any) => console.error(err, 'graylog'));
     }
 
     public emit(logOptions: LogOptions) {
         if (this.hideLog) return;
         let logMessage = this.formatMessage(logOptions);
+        console.log('GO TO GELF')
         this.gelf.emit('gelf.log', logMessage);
     }
 
@@ -23,6 +27,50 @@ export class GreenLogs {
         delete options.additionalColumns
         return options
     }
+}
+
+
+//@Decorators
+export function LogApplication(target: any, propertyKey: string, descriptor: PropertyDescriptor): any {
+    let originalMethod = descriptor.value;
+    descriptor.value = function (...args: any[]): any {
+        console.log('Running Method: ' + originalMethod.name)
+        const result: any = originalMethod.apply(this, args);
+        let msg = messageReturnOfFunction(result)
+        console.table(msg)
+        return result
+    };
+}
+
+//@Decorators
+export function LogGrayLog(target: any, propertyKey: string, descriptor: PropertyDescriptor): any {
+    let log = GetObjectLog()
+    let originalMethod = descriptor.value;
+    descriptor.value = function (...args: any[]): any {
+        const result: any = originalMethod.apply(this, args);
+        let message: any = {
+            short_message: 'Running Method: ' + originalMethod.name,
+            Method: originalMethod.name,
+            Result: messageReturnOfFunction(result)
+        }
+        log.emit(message)
+    };
+}
+
+function messageReturnOfFunction(resultMethod: any) {
+    if (resultMethod instanceof Promise) return 'Method return: <Promise>'
+    if (!resultMethod) return 'No return to the method.'
+    if (Array.isArray(resultMethod)) return 'Method returning list with the size of: ' + resultMethod.length
+    return `Method return: ${JSON.stringify(resultMethod)}`
+}
+
+var logContext: Gelf
+function SetObjectLog(context: Gelf) {
+    logContext = context
+}
+
+function GetObjectLog() {
+    return logContext
 }
 
 
@@ -41,24 +89,15 @@ interface Columns {
 }
 
 
-//@Decorators
-export function LogApplication(target: any, propertyKey: string, descriptor: PropertyDescriptor): any {
-    let originalMethod = descriptor.value;
-    descriptor.value = async function (...args: any[]): Promise<any> {
-        console.log('Running Method: ', originalMethod.name)
-        const result: any = await originalMethod.apply(this, args);
-        if (!result) {
-            console.log('No return to the method.')
-            return
-        }
 
-        if (Array.isArray(result)) {
-            console.log('Method returning list with the size of: ', result.length)
-            return
-        }
-        console.log('Method return: ', result)
-    };
-}
+
+
+//TODO LIST:
+/*
+[]-no que impacta nos testes o decorator/emit
+[]-CIRCLE CI (https://circleci.com/)
+*/
+
 
 
 
